233ac8fa5d912af1ee8917bc2168b53ed2b4b064
Winterson <u19cse1028@cit.ac.in>
2023-05-03 00:34:25 +0530

    BOX

--- src/main.rs
+++ src/main.rs
@@ -146,4 +146,32 @@ fn main() {
             _other_error => panic!("Problem opening file: {:?}", error),
         },
     };
+
+    // BOX
+    struct TreeNode<T> {
+        pub left: Option<Box<TreeNode<T>>>,
+        pub right: Option<Box<TreeNode<T>>>,
+        pub key: T,
+    }
+    impl<T> TreeNode<T> {
+        pub fn new(key: T) -> Self {
+            TreeNode {
+                left: None,
+                right: None,
+                key,
+            }
+        }
+        pub fn left(mut self, node: TreeNode<T>) -> Self {
+            self.left = Some(Box::new(node));
+            self
+        }
+        pub fn right(mut self, node: TreeNode<T>) -> Self {
+            self.right = Some(Box::new(node));
+            self
+        }
+    }
+    // Creating Binary Tree
+    let node1 = TreeNode::new(1)
+        .left(TreeNode::new(2))
+        .right(TreeNode::new(3));
 }
